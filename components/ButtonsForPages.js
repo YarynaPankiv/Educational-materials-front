f7bd9505f96ae3739afb2820a6bd3b9f803e8b7e
YarynaPankiv <pankiv.yaryna00@gmail.com>
2024-04-10 16:57:15 +0300

    Кнопки після товарів для перемикання

---
+++ components/ButtonsForPages.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import Center from "./Center";
+
+const StyledButton = styled.div`
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+    border-radius: 10px;
+    background: ${props => props.clicked === 'true' ? "#AD88C6" : "#FCFAFA"}; /* змінив тут */
+    border: 1px solid #000000;
+    color: ${props => props.clicked === 'true' ? "white" : "black"}; /* змінив тут */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 8px;
+`;
+
+const StyledLineOfButtons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 37px;
+`;
+
+export default function ButtonsForPages({ totalPages, onPageChange, currentPage }) {
+    const handleButtonClick = (pageNumber) => {
+        onPageChange(pageNumber);
+    };
+
+    const renderNumberedButtons = () => {
+        const buttons = [];
+        for (let i = 1; i <= totalPages; i++) {
+            buttons.push(
+                <StyledButton key={i} onClick={() => handleButtonClick(i)} clicked={i === currentPage ? 'true' : 'false'}>
+                    {i}
+                </StyledButton>
+            );
+        }
+        return buttons;
+    };
+
+    return (
+        <Center>
+            <StyledLineOfButtons>
+                {renderNumberedButtons()}
+            </StyledLineOfButtons>
+        </Center>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Buttons from "@/components/Buttons";
 import Header from "@/components/Header";
 import Logo from "@/components/Logo";
 import ProductsGrid from "@/components/ProductsGrid";
+import ButtonsForPages from '@/components/ButtonsForPages';
 
 export default function HomePage() {
     const originalProducts = ([
@@ -31,6 +32,37 @@ export default function HomePage() {
             image: "/image.png",
             price: 200,
             description: "Опис продукту 2"}
+            ,
+
+          { id: 5,
+            name: "Назва продукту 5",
+            image: "/image.png",
+            price: 200,
+            description: "Опис продукту 2"},
+
+            { id: 6,
+              name: "Назва продукту 6",
+              image: "/image.png",
+              price: 200,
+              description: "Опис продукту 2"},
+
+              { id: 7,
+                name: "Назва продукту 7",
+                image: "/image.png",
+                price: 200,
+                description: "Опис продукту 2"},
+
+                { id: 8,
+                  name: "Назва продукту 8",
+                  image: "/image.png",
+                  price: 200,
+                  description: "Опис продукту 2"},
+
+                  { id: 9,
+                    name: "Назва продукту 9",
+                    image: "/image.png",
+                    price: 200,
+                    description: "Опис продукту 2"}
     ]);
     const [products, setProducts] = useState(originalProducts);
 
@@ -38,18 +70,35 @@ export default function HomePage() {
         const sortedProducts = originalProducts.sort((a, b) => b.id - a.id);
         const recentlyAdded = sortedProducts.slice(0, 10);
         setProducts(recentlyAdded);
+        setCurrentPage(1); }
+    
+    const handleAllProductsClick = () => {
+        setProducts(originalProducts);
+        setCurrentPage(1);
+    }
+    
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 8;
+    const totalPages = Math.ceil(products.length / productsPerPage);
+    
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
     };
 
-    const handleAllProductsClick = () => {
-      setProducts(originalProducts);
-    }
+    const startIndex = (currentPage - 1) * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+    const displayedProducts = products.slice(startIndex, endIndex);
 
     return (
         <div>
             <Header />
             <Logo />
             <Buttons onRecentlyAddedClick={handleRecentlyAddedClick} onAllClick={handleAllProductsClick} />
-            <ProductsGrid products={products} />
+            <ProductsGrid products={displayedProducts} />
+            <ButtonsForPages totalPages={totalPages} onPageChange={handlePageChange} currentPage={currentPage} />
+
         </div>
     );
 }
+    
