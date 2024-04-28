20685e6014901334dadb04f5bcbfce10e87f4d25
Den Lehenkyi <denlehenkyi@gmail.com>
2024-04-28 16:35:27 +0300

    commit

--- pages/Checkout.js
+++ pages/Checkout.js
@@ -10,14 +10,16 @@ import PayButton from "@/components/Buttons/PayButton";
 import { getFileExtension } from "./product/[id]";
 import PaymentButton from "@/components/Buttons/PaymentButton";
 import Header from "@/components/Header";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import Center from "@/components/Center";
 import LogoWithoutPurple from "@/components/Logo/LogoWithoutPurple";
 import Urls from "@/components/Urls";
 import { Category } from "@/models/Category";
 import SubCategory from "@/models/SubCategory";
+import { useState } from "react";
+import { useEffect } from "react";
 
-// Створюємо стилі для контейнера замовлення
+
 const OrderContainer = styled.div`
   margin: 20px auto;
   width: 650px;
@@ -80,9 +82,8 @@ const ProductOrder = styled.div`
   background-color: white;
   margin-bottom: 10px;
   @media only screen and (max-width: 600px) {
-    width:auto;
-    height:auto
-    
+    width: auto;
+    height: auto;
   }
 `;
 const StyledH2 = styled.h2`
@@ -101,24 +102,35 @@ const PaymentMessage = styled.h1`
   font-family: "Montserrat";
   font-style: normal;
   font-weight: bolder;
-  margin-top:90px;
+  margin-top: 90px;
   font-size: 32px;
   @media only screen and (max-width: 650px) {
     width: 100%;
     margin-top: 20px;
   }
-`
+`;
 const SideAlignedWrapper = styled.div`
   display: flex;
-  justify-content: center; 
+  justify-content: center;
 `;
 const StyledP = styled.p`
-   font-size: 18px;
+  font-size: 18px;
 `;
-export default function Checkout({categories, subcategories}) {
-    const router = useRouter();
-  const { cartProducts, setCartProducts, deleteProductFromCart } =
-    useContext(CartContext);
+export default function Checkout({ categories, subcategories }) {
+  const router = useRouter();
+  const { cartProducts, deleteProductFromCart } = useContext(CartContext);
+  const [fileDownloaded, setFileDownloaded] = useState(false);
+
+  useEffect(() => {
+    if (router.query.success === "true" && !fileDownloaded) {
+      const fileInCart = cartProducts.find(
+        (pr) => pr.file && pr.file.length > 0
+      )?.file[0]?.url;
+      if (fileInCart) {
+        handleFileDownload(fileInCart);
+      }
+    }
+  }, [router.query.success, cartProducts, fileDownloaded]);
 
   const calculateTotal = () => {
     let total = 0;
@@ -127,111 +139,89 @@ export default function Checkout({categories, subcategories}) {
     });
     return total;
   };
+
   const totalCost = calculateTotal();
 
-  if (router.query.success === 'true') {
-    let fileInCart;
-  
-    cartProducts.map((pr) => {
-        if (pr.file && pr.file.length > 0) {
-            fileInCart = pr?.file[0]?.url;
-        }
-    });
-    console.log(cartProducts);
-    console.log("FILEEEEEEEE", fileInCart);
-    const handleFileDownload = () => {
-        const fileURL = fileInCart;
-        const link = document.createElement('a');
+  const handleFileDownload = (fileURL) => {
+    const link = document.createElement("a");
+    link.href = fileURL;
+    link.setAttribute("download", true);
+    link.click();
+    setFileDownloaded(true);
+  };
 
-        link.href = fileURL;
-
-        document.body.appendChild(link);
-       
-        link.click();
-      
-        document.body.removeChild(link);
-    };
-   
-    handleFileDownload();
-
-    return(
-        <>
-        <Header subcategories={subcategories} categories={categories}/>
-        <PaymentMessage>Оплата пройшла успішно!</PaymentMessage>      
-        </>
-        );
-    
+  if (router.query.success === "true") {
+    return (
+      <>
+        <Header subcategories={subcategories} categories={categories} />
+        <PaymentMessage>Оплата пройшла успішно!</PaymentMessage>
+      </>
+    );
   }
 
-  if (router.query.success === 'false') {
-    return(
-        <>
-        <Header>
+  if (router.query.success === "false") {
+    return (
+      <>
+        <Header subcategories={subcategories} categories={categories} />
         <PaymentMessage>Оплата не пройшла успішно!</PaymentMessage>
-        </Header>
-        </>
-        );
-    
+      </>
+    );
   }
+
   return (
     <Center>
       <Header subcategories={subcategories} categories={categories} />
-      <Urls page={"Оформлення замовлення"}/>
+      <Urls page="Оформлення замовлення" />
       <SideAlignedWrapper>
-      <OrderContainer>
-      <StyledH2>Оформлення замовлення</StyledH2>
-      {cartProducts &&
-        cartProducts.map((pr) => (
-          <ProductOrder key={pr._id}>
-            <IconTrash onClick={() => deleteProductFromCart(pr._id)}>
-              <svg
-                width="15"
-                height="17"
-                viewBox="0 0 15 17"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2.72396 16.4271C2.25868 16.4271 1.86181 16.2632 1.53333 15.9354C1.20556 15.6076 1.04167 15.2108 1.04167 14.7448V1.84375H0V0.802083H4.16667V0H10.4167V0.802083H14.5833V1.84375H13.5417V14.7448C13.5417 15.224 13.3812 15.624 13.0604 15.9448C12.7396 16.2656 12.3392 16.4264 11.8594 16.4271H2.72396ZM12.5 1.84375H2.08333V14.7448C2.08333 14.9316 2.1434 15.0851 2.26354 15.2052C2.38368 15.3253 2.53715 15.3854 2.72396 15.3854H11.8594C12.0191 15.3854 12.166 15.3187 12.3 15.1854C12.434 15.0521 12.5007 14.9052 12.5 14.7448V1.84375ZM5.00833 13.3021H6.05V3.92708H5.00833V13.3021ZM8.53333 13.3021H9.575V3.92708H8.53333V13.3021Z"
-                  fill="#DF0F0F"
-                />
-              </svg>
-            </IconTrash>
-
-            <ProductImageWrapper>
-              <StyledImage src={pr.images[0]} />
-              <ul>
-                <StyledP key={pr._id}>
-                  <b>{pr.productName}</b>
-                </StyledP>
-                <StyledP key={pr._id}>
-                  Формат:{" "}
-                  <PurpleText>
-                    {pr.file && pr.file[0] && pr.file[0].name
-                      ? getFileExtension(pr.file[0].name)
-                      : ""}
-                  </PurpleText>
-                </StyledP>
-                <p key={pr._id}>
-                  <StyledCost>{pr.price} ГРН</StyledCost>
-                </p>
-              </ul>
-            </ProductImageWrapper>
-          </ProductOrder>
-        ))}
-      {totalCost > 0 && (
-        <TotalCost>
-          Загальна вартість: <GreenPrice>{totalCost} ГРН</GreenPrice>
-        </TotalCost>
-      )}
-      <PaymentButton></PaymentButton>
-    </OrderContainer>
-
+        <OrderContainer>
+          <StyledH2>Оформлення замовлення</StyledH2>
+          {cartProducts &&
+            cartProducts.map((pr) => (
+              <ProductOrder key={pr._id}>
+                <IconTrash onClick={() => deleteProductFromCart(pr._id)}>
+                  <svg
+                    width="15"
+                    height="17"
+                    viewBox="0 0 15 17"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2.72396 16.4271C2.25868 16.4271 1.86181 16.2632 1.53333 15.9354C1.20556 15.6076 1.04167 15.2108 1.04167 14.7448V1.84375H0V0.802083H4.16667V0H10.4167V0.802083H14.5833V1.84375H13.5417V14.7448C13.5417 15.224 13.3812 15.624 13.0604 15.9448C12.7396 16.2656 12.3392 16.4264 11.8594 16.4271H2.72396ZM12.5 1.84375H2.08333V14.7448C2.08333 14.9316 2.1434 15.0851 2.26354 15.2052C2.38368 15.3253 2.53715 15.3854 2.72396 15.3854H11.8594C12.0191 15.3854 12.166 15.3187 12.3 15.1854C12.434 15.0521 12.5007 14.9052 12.5 14.7448V1.84375ZM5.00833 13.3021H6.05V3.92708H5.00833V13.3021ZM8.53333 13.3021H9.575V3.92708H8.53333V13.3021Z"
+                      fill="#DF0F0F"
+                    />
+                  </svg>
+                </IconTrash>
+                <ProductImageWrapper>
+                  <StyledImage src={pr.images[0]} />
+                  <ul>
+                    <StyledP key={pr._id}>
+                      <b>{pr.productName}</b>
+                    </StyledP>
+                    <StyledP key={pr._id}>
+                      Формат:{" "}
+                      <PurpleText>
+                        {pr.file && pr.file[0] && pr.file[0].name
+                          ? getFileExtension(pr.file[0].name)
+                          : ""}
+                      </PurpleText>
+                    </StyledP>
+                    <p key={pr._id}>
+                      <StyledCost>{pr.price} ГРН</StyledCost>
+                    </p>
+                  </ul>
+                </ProductImageWrapper>
+              </ProductOrder>
+            ))}
+          {totalCost > 0 && (
+            <TotalCost>
+              Загальна вартість: <GreenPrice>{totalCost} ГРН</GreenPrice>
+            </TotalCost>
+          )}
+          <PaymentButton></PaymentButton>
+        </OrderContainer>
       </SideAlignedWrapper>
-      
-
     </Center>
- 
   );
 }
 
